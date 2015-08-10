1a50c8ec1cb75c5cf00b93983951609f5eb51c1d
ngocthangict <ngocthang.ict@gmail.com>
2015-08-10 07:51:18 +0700

    Css compre, wishlist button

--- wp-content/themes/kutetheme/functions.php
+++ wp-content/themes/kutetheme/functions.php
@@ -288,6 +288,7 @@ function kt_scripts() {
         ), '1.0' );
         // Load our main stylesheet.
 	wp_enqueue_style( 'kutetheme-style', get_stylesheet_uri(),array('kt-style') );
+	wp_enqueue_style( 'custom-woocommerce-style', get_template_directory_uri().'/css/woocommerce.css',array('kt-style') );
             
     //wp_enqueue_style( 'kt-option-6', get_template_directory_uri() . '/css/option6.css', array('kt-style') );
     
